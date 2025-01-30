104e16774ead6c7c2a55b1ed015d621e0529e48f
Thierry Steenhoudt <amaroknt@outlook.fr>
2025-01-30 15:59:52 +0100

    ajout tokken

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -7,7 +7,8 @@ const userSchema = new Schema({
     email : {type: String, required: true},
     password : {type: String, required: true},
     lobbies : {type: [String] , default: []},  // {type: [{type: mongoose.SchemaTypes.ObjectId, ref: "Lobby"}] , default: []}, 
-    createdAt : {type: Date, default: Date.now()}
+    createdAt : {type: Date, default: Date.now()},
+    token: {type: String, required:true}
 })
 
 module.exports = mongoose.model("User", userSchema);
