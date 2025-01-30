6301a758c3d049260f1d11939f8c2c4101038567
guclugurkan <guclugrkn@gmail.com>
2025-01-30 14:06:12 +0100

    Ajout dashboard

--- frontend/lokkeroom-frontend/src/App.jsx
+++ frontend/lokkeroom-frontend/src/App.jsx
@@ -3,14 +3,20 @@ import './App.css'
 import Home from "./pages/home";
 import SignUp from "./pages/signUp";
 import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Lobbys from "./pages/lobbys";
+import NewLobby from "./pages/newLobby";
 
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/signUp" element={<SignUp />} />
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/lobbys" element={<Lobbys />} />
+      <Route path="/newLobby" element={<NewLobby />} />
       </Routes>
     </Router>
   )
